88811f2de879f86def9bbbae25c5b5a660813d73
ShaikZakriya <155053622+ShaikZakriya@users.noreply.github.com>
2025-04-24 01:42:40 +0530

    Initial commit

---
+++ src/main/java/Java/Operators.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		Arithmetic_operators();
+
+	}
+
+	private static void Arithmetic_operators() {
+		
+		
+		
+	}
+
+}
